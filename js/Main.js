b793ccd78ec95ffd7cebbf8d60f23e72a031acd7
weng1ii <shandabylosasha@gmail.com>
2023-03-06 21:22:28 +0300

    Change ripper effect

---
+++ js/Main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+"use strict"
+	$('#home').ripples({
+	  dropRadius: 13,
+	  perturbance: 0.01,
+
+	});
+
+	$("#effecttyped").typed({
+	  strings:["Головна","Допоможіть"],
+	  typespeed:0,
+	  loop:true
+	});	
+});
